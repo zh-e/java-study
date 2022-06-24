f873ebd8bf816937539f9893bc87af372633a74a
zh-e <zz950702@163.com>
2022-06-25 00:08:34 +0800

    一个小问题

--- java-base/src/main/java/com/zhe/javabase/Main.java
+++ java-base/src/main/java/com/zhe/javabase/Main.java
@@ -1,35 +1,14 @@
 package com.zhe.javabase;
 
-import org.xxtea.XXTEA;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-
 public class Main {
 
-    private static final String KEY = "hall.58hld-YBHD-PK";
+    public static void main(String[] args) {
 
-    private static final String KEY_1 = "gcf3E3uDI&tCA&2u";
+        String a = "12131";
 
-
-    public static void main(String[] args) throws UnsupportedEncodingException {
-
-        String str = "Bgn//f0C1TfbUar3FiOr8bSSiQJUtFWk02YkIoz5JyswVDJsFmxlgF8XsRLHiKr8iV73lfQmfsn52VroIZrLnQQo3vJhAH7gSVTLdmBFUUizf/tVYP6UrBcsXoR+de00BiWWZtmecFxMA/FKtdeppA==";
-        System.out.println(req(str));
-    }
-
-    public static String req(String str) {
-        return XXTEA.decryptBase64StringToString(str, KEY);
-    }
-
-    public static String resp(String str) {
-        try {
-            String a = URLDecoder.decode(str, "utf-8");
-            return XXTEA.decryptBase64StringToString(a, KEY);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(a, 0, 1);
+        System.out.println(sb.toString());
     }
 
 }
